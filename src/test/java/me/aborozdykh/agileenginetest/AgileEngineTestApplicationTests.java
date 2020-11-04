731281c74e9a10018723bbfe4334d838d462ed0c
Andrii Borozdykh <aborozdykh@gmail.com>
2020-11-04 11:39:47 +0200

    Initial commit

---
+++ src/test/java/me/aborozdykh/agileenginetest/AgileEngineTestApplicationTests.java
@@ -0,0 +1,13 @@
+package me.aborozdykh.agileenginetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgileEngineTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
